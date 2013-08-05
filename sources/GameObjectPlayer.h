22e7537e04eacf589c3b2beaa2672f54521a1f7d
Matt Cleveland <mgcleveland@gmail.com>
2013-08-05 14:00:03 -0700

    Revert 120.0->6.0
    Signed-off-by: Matt Cleveland <mgcleveland@gmail.com>

--- sources/GameObjectPlayer.h
+++ sources/GameObjectPlayer.h
@@ -14,7 +14,7 @@
 #define kPlayerHitBoxSegmentWidth kPlayerSpeed
 #define PLAYER_IDLE_TICKS  5//15
 #define JUST_STARTED_MOVING_THRESHOLD 5//10
-#define EXTREMITY_RING_IDLE_THRESHOLD 120.0
+#define EXTREMITY_RING_IDLE_THRESHOLD 6.0
 
 typedef enum
 {
